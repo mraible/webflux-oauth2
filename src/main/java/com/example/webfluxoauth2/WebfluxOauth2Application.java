ad457fc3909e0a15937fee09b207c0033b96d672
Matt Raible <matt.raible@okta.com>
2018-05-12 16:45:12 -0600

    Initial commit

---
+++ src/main/java/com/example/webfluxoauth2/WebfluxOauth2Application.java
@@ -0,0 +1,12 @@
+package com.example.webfluxoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxOauth2Application.class, args);
+	}
+}
